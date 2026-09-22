7bc6398448077e2e6eca129262888a9ba7c4be72
2601110284-cyber <2601110283@gspace.kopo.ac.kr>
2026-09-22 14:15:08 +0900

    11장 파일 입출력(전체 파일 내용 반대로 출력)

---
+++ src/ai0922/LAB_AddLineNumber.java
@@ -0,0 +1,36 @@
+package ai0922;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LAB_AddLineNumber {
+    static void main(String[] args) {
+
+        try{
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader((new FileReader("myDataTest2.txt")));
+            String line = "";
+            int cnt = 1;
+
+            while (true){
+                line = br.readLine();
+                if (line == null)
+                    break;
+                System.out.println(cnt++ + " : "+line);;
+            }
+
+            br.close();
+
+//            sb.reverse();
+//            System.out.println(sb);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    }
+
